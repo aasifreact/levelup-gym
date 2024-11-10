a63950e16160be4f2ef7d8050145369dc6439329
aasifreact <aasifreact@gmail.com>
2024-11-10 18:08:05 +0530

    updated time span of loader

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 5000);
+    }, 300);
 
     return () => clearTimeout(timer);
   }, []);
